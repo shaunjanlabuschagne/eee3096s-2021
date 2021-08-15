5113704c1a1f74534f7995f1bbeace592b2f5187
divy17 <69718338+divy17@users.noreply.github.com>
2021-08-15 13:22:32 +0200

    Add files via upload

---
+++ Tutorials/Tut 1/main2.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main() {
+            int a, b, sum;
+            printf("Entr a value for a: ");
+            scanf("%d", &a);
+            printf("Enter a value for b: ");
+            scanf("%d", &b);
+            sum = a + b ;
+            printf("The sum of a and b is %d \n.", sum);
+
+
+

4fd2fae1f2f029ffec32f5303b875516d28cb844
Shaun <shaunlabuschagne902@gmail.com>
2021-08-20 18:59:05 +0100

    code working now

---
+++ Practicals/Prac-01/src/CurrentTime.c
@@ -0,0 +1,30 @@
+#include "CurrentTime.h"
+#include <time.h>
+
+int HH,MM,SS;
+
+void getCurrentTime(void){
+  time_t rawtime;
+  struct tm * timeinfo;
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+
+  HH = timeinfo ->tm_hour;
+  MM = timeinfo ->tm_min;
+  SS = timeinfo ->tm_sec;
+}
+
+int getHours(void){
+    getCurrentTime();
+    return HH;
+}
+
+int getMins(void){
+    getCurrentTime();
+    return MM;
+}
+
+int getSecs(void){
+    getCurrentTime();
+    return SS;
+}
